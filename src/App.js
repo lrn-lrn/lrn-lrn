e0883b10c5c0ae68a29d2b0d645442dc944c219c
Sarah Joy Calpo <sajoy.pc@gmail.com>
2017-08-22 11:34:51 -0700

    display buttons

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Card from './Card.js';
+import Button from './Button.js';
 
 class App extends Component {
   render() {
     return (
       <section id="container">
+        <Button text="IDK"/>
         <Card/>
+        <Button text="YES"/>
       </section>
     );
   }
