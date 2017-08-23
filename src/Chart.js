f53913d93d1ba35bc939153114671ce33c90e95e
Morgan Fogarty <mofo37@gmail.com>
2017-08-23 11:53:59 -0700

    times displayed human friendlier

--- src/Chart.js
+++ src/Chart.js
@@ -4,7 +4,7 @@ import SessionRow from './SessionRow.js';
 
 const Chart = ({words, sessions}) => (
   <table>
-    <WordHeader words={words} />
+    <WordHeader words={words}/>
     <tbody>
       {Object.keys(sessions).map((key, i) => (
         <SessionRow session={sessions[key]} time={key}/>
--- src/SessionRow.js
+++ src/SessionRow.js
@@ -3,12 +3,17 @@ import React from 'react';
 const SessionRow = ({session, time}) => (
     <tr>
         <th>
-            {time}
+            {formatTime(time)}
         </th>
             {session.map( ( response, i ) => (
                 <td key={i}>{response.toString()}</td>
             ))}
     </tr>
 );
+
+function formatTime(time) {
+    var date = new Date(parseInt(time));
+    return date.toDateString() + ' @ ' + date.toLocaleTimeString();
+}
 
 export default SessionRow;
